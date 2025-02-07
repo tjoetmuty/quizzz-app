7376341c4794a26e25c7d06645f4875209e7eae1
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-02-07 21:46:27 +0800

    chore(quest): create several questions

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,31 @@
+import { QuestionsType } from "@/types";
+
+export const QuestionsData: QuestionsType[] = [
+  {
+    id: 1,
+    question: "JavaScript is an ______ language",
+    options: [
+      'Object-Oriented', 'Object-Based', 'Procedural'
+    ]
+  },
+  {
+    id: 2,
+    question: "Which type of JavaScript language is ___?",
+    options: ["Object-Oriented", "Object-Based", "Assembly-language", "High-level"],
+  },
+  {
+    id: 3,
+    question: "Which of the following keywords is used to define a variable in JavaScript?",
+    options: ["var", "let", "const", "All of the above"],
+  },
+  {
+    id: 4,
+    question: "Inside which HTML element do we put the JavaScript code?",
+    options: ["<script>", "<javascript>", "<js>", "<scripting>"],
+  },
+  {
+    id: 5,
+    question: "Which function is used to print content in JavaScript?",
+    options: ["console.log()", "print()", "display()", "log()"],
+  },
+]
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,5 @@
+export interface QuestionsType {
+  id?: number
+  question?: string
+  options?: string[]
+}
