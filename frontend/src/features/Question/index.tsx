17cee66f6bfa2ce25e495868c1f3612a2d1b6a1e
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-02-07 22:35:00 +0800

    add question

--- frontend/src/features/Question/index.tsx
+++ frontend/src/features/Question/index.tsx
@@ -1,30 +1,38 @@
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { FC } from "react";
+import { QuestionsData } from "@/constants";
+import { FC, useEffect, useState } from "react";
 
 const QuestionFeat: FC = () => {
+  const [checked, setChecked] = useState(undefined);
+  const Question = QuestionsData[0]
   const onSelect = () => {
     console.log("radio button selected");
   };
+  useEffect(() => {
+    console.log("quests", QuestionsData);
+  }, []);
+
   return (
     <div>
       <div className="flex flex-col gap-2">
-        <h2 className="text-white font-bold text-lg">Sample Question 1</h2>
+        <h2 className="text-white font-bold text-lg">{Question.question}</h2>
         <div className="text-white">
-          <RadioGroup defaultValue="comfortable">
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="answer 1" id="r1" name="options" onChange={onSelect()}/>
-              <Label htmlFor="r1">answer 1</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="answer 2" id="r2" name="options" onChange={onSelect()}/>
-              <Label htmlFor="r2">answer 2</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="answer 3" id="r3" name="options" onChange={onSelect()}/>
-              <Label htmlFor="r3">answer 3</Label>
-            </div>
-          </RadioGroup>
+            <RadioGroup defaultValue="comfortable" >
+              <div className="grid gap-4">
+                {Question.options?.map((opt, i) => (
+                  <div key={i} className="flex gap-4">
+                    <RadioGroupItem
+                      value="answer 1"
+                      id={`q${i}-options`}
+                      name="options"
+                      onChange={onSelect()}
+                    />
+                    <Label htmlFor={`q${i}-options`}>{opt}</Label>
+                  </div>
+                ))}
+              </div>
+            </RadioGroup>
         </div>
       </div>
     </div>
