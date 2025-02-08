778c68bf1ab484744a9f0c7d577d3595993e31dc
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-02-08 21:52:36 +0800

    fix(main): button position

--- frontend/src/features/Main/index.tsx
+++ frontend/src/features/Main/index.tsx
@@ -7,12 +7,12 @@ const MainFeat: FC = () => {
     <div className="bg-gray-900">
       <div className="py-10 mx-auto h-screen block relative w-1/4">
         <h1 className="text-white font-extrabold text-lg text-center ">Quiz Application</h1>
-        <form className="">
           <div className="grid justify-center pt-8 gap-4 ">
+        <form className="">
           <input type="text" placeholder="username" className="p-2 border-none rounded-md" />
-          </div>
         </form>
             <button onClick={() => navigate("/quiz")} className="bg-yellow-300 font-medium px-6 py-1 rounded-md text-md">start quiz</button>
+          </div>
       </div>
     </div>
   );
