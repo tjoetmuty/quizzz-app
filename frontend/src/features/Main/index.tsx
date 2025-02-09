e27597769c94250737b60c24aa82eec614ea1b77
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-02-09 18:08:54 +0800

    feat(main): fix styling

--- frontend/src/features/Main/index.tsx
+++ frontend/src/features/Main/index.tsx
@@ -5,14 +5,25 @@ const MainFeat: FC = () => {
   const navigate = useNavigate();
   return (
     <div className="bg-gray-900">
-      <div className="py-10 mx-auto h-screen block relative w-1/4">
-        <h1 className="text-white font-extrabold text-lg text-center ">Quiz Application</h1>
-          <div className="grid justify-center pt-8 gap-4 ">
-        <form className="">
-          <input type="text" placeholder="username" className="p-2 border-none rounded-md" />
-        </form>
-            <button onClick={() => navigate("/quiz")} className="bg-yellow-300 font-medium px-6 py-1 rounded-md text-md">start quiz</button>
-          </div>
+      <div className="py-10 mx-auto h-screen block relative w-1/4 container">
+        <h1 className="text-white font-extrabold text-3xl text-center border-4 border-green-300 p-4">
+          Quiz Application
+        </h1>
+        <div className="flex flex-col justify-center pt-8 gap-4">
+          <form className="">
+            <input
+              type="text"
+              placeholder="Username"
+              className="p-2 border-none rounded-md w-full"
+            />
+          </form>
+          <button
+            onClick={() => navigate("/quiz")}
+            className="bg-yellow-300 font-medium px-6 py-1 rounded-md text-md"
+          >
+            Start Quiz
+          </button>
+        </div>
       </div>
     </div>
   );
